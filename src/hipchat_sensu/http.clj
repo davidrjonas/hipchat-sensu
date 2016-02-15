c24e94ae08b3978554e2765605d337e6ba28bd85
David Jonas <djonas@noip.com>
2016-02-15 03:26:53 -0800

    Improve http/response->json-params to handle error responses

--- src/hipchat_sensu/http.clj
+++ src/hipchat_sensu/http.clj
@@ -1,5 +1,6 @@
 (ns hipchat-sensu.http
   (:require [org.httpkit.client :as http]
+            [taoensso.timbre :as log]
             [clojure.data.json :as json]))
 
 (defn post-json [url json-data bearer-token]
@@ -10,11 +11,13 @@
                   :body (json/write-str json-data)}))
 
 (defn- response->json-params [response]
-  (if (>= (:status response) 400)
-    {:error "failed to GET url"}
-    (try
-      (-> response :body (json/read-str :key-fn keyword))
-      (catch java.io.EOFException e {:error "invalid json"}))))
+  (cond
+    (:error response) response
+    (>= (:status response) 400) {:error "failed to GET url"}
+    :else
+      (try
+        (-> response :body (json/read-str :key-fn keyword))
+        (catch java.io.EOFException e {:error "invalid json"}))))
 
 (defn get-json [url]
   (let [response @(http/get url)]
