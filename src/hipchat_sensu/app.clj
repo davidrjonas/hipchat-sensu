384628abe96e26ddb4e68265d65a20b2bd58d605
David Jonas <djonas@noip.com>
2016-02-15 01:17:35 -0800

    Split handler into app and handler files

---
+++ src/hipchat_sensu/app.clj
@@ -0,0 +1,47 @@
+(ns hipchat-sensu.app
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [taoensso.timbre :as log]
+            [hipchat-sensu.handler :as handler]
+            [ring.util.response :refer [response]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.middleware.json :refer [wrap-json-params wrap-json-response]]))
+
+; Use env to set vars
+(def baseurl "http://localhost")
+
+(def method->str {:head "HEAD" :get "GET" :post "POST" :put "PUT" :patch "PATCH"})
+
+(defn wrap-logger [handler]
+  (fn [request]
+    (let [response (handler request)]
+      (log/info (:remote-addr request)
+                (str "\"" (-> request :request-method method->str) " " (:uri request) "\"")
+                (:status response))
+      (log/debug request)
+      response)))
+
+(defroutes app-routes
+  (GET "/" [] "HipChat Sensu Addon")
+
+  (GET "/capabilities.json"
+       req (wrap-json-response
+             (fn [req] (response (handler/capabilities baseurl)))))
+
+  (POST "/installed"
+        req (handler/installed (:params req)))
+
+  (POST "/uninstalled"
+        req (handler/uninstalled (:params req)))
+
+  (POST "/glance-data"
+        req (wrap-json-response handler/glance-data))
+
+  (route/not-found "Not Found"))
+
+(def app
+  (-> app-routes
+      (wrap-defaults api-defaults)
+      wrap-logger
+      wrap-json-params))
+
--- src/hipchat_sensu/handler.clj
+++ src/hipchat_sensu/handler.clj
@@ -1,11 +1,53 @@
 (ns hipchat-sensu.handler
-  (:require [compojure.core :refer :all]
-            [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+  (:require [taoensso.timbre :as log]
+            [clojure.data.json :as json]
+            [hipchat-sensu.installation :as inst]
+            [hipchat-sensu.access-token :as tok]
+            [hipchat-sensu.http :as http]
+            [hipchat-sensu.sensu :as sensu]
+            [hipchat-sensu.hipchat :as hipchat]
+            [ring.util.response :refer [response redirect]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.middleware.json :refer [wrap-json-params wrap-json-response]]))
 
-(defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+(defn capabilities [baseurl]
+  {:name "Sensu HipChat AddOn"
+   :description "Sensu glances and side-view"
+   :key ""
+   :vendor {:name "No-IP" :url "http://www.noip.com"}
+   :links {:homepage "http://gitlab.lax/djonas/hipchat-sensu-addon"
+           :self (str baseurl "/capabilities.json")}
+   :capabilities {:hipchatApiConsumer {}
+                  :installable {:allowGlobal true
+                                :allowRoom true
+                                :callbackUrl (str baseurl "/installed")
+                                :uninstalledUrl (str baseurl "/uninstalled")}
+                  :glance [{:icon {:url ""
+                                   "url@2x" ""}
+                           :key "sensu-glance"
+                           :name {:value "Sensu Glance"}
+                           :queryUrl (str baseurl "/glance-data")
+                           :target "sensu-sidebar"}]}})
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+
+(defn installed [params]
+  (let [capa (:params (http/get-json (:capabilitiesUrl params)))
+        params (assoc params :capabilities capa)
+        id (:oauthId params)]
+    (inst/add id params)
+    (sensu/watch id hipchat/on-sensu-events)
+    (log/info "installed: " params))
+  {:status 200})
+
+(defn uninstalled [params]
+  (let [redirectUrl (:redirect-url params)
+        installableUrl (:installable-url params)
+        installation (:params (http/get-json installableUrl))]
+    (-> installation :oauthId inst/delete tok/delete)
+    (log/info "uninstalled: " params ", installation: " installation)
+    (redirect redirectUrl)))
+
+(defn glance-data [request]
+  (comment validate-jwt)
+  (comment ensure cors)
+  (response (hipchat/events->glance-data (sensu/current))))
