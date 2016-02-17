384d2b004d04f7fd27170af5a27d379431a0dc0f
David Jonas <djonas@noip.com>
2016-02-16 18:39:48 -0800

    Fix syntax error, improve code in hipchat

--- src/hipchat_sensu/hipchat.clj
+++ src/hipchat_sensu/hipchat.clj
@@ -1,38 +1,28 @@
 (ns hipchat-sensu.hipchat
   (:require [taoensso.timbre :as log]
-            [clojure.data.json :as json]
+            [clojure.string :as s]
             [hipchat-sensu.installation :as inst]
             [hipchat-sensu.access-token :as tok]
             [hipchat-sensu.http :as http]
-            [hipchat-sensu.sensu :as sensu]
-            [ring.util.response :refer [response redirect]]))
+            [ring.util.response :refer [response redirect get-header]]))
 
-(comment
-(defn- validate-jwt [oauth-id token]
-  (let [secret (-> oauth-id inst/lookup :oauthSecret)
-        jwt (str->jwt token )]
-    (verify jwt secret) (:claims jwt)))
-)
+(def status->kw {0 :ok 1 :warn 2 :crit})
 
-(def ^:private status->kw {0 :ok 1 :warn 2 :crit})
-
-(defn- label-value [statuses]
+(defn label-value [statuses]
   (let [warn-str (if (:warn statuses) (str "<b>" (:warn statuses) "</b>W ") "")
         crit-str (if (:crit statuses) (str "<b>" (:crit statuses) "</b>C ") "")]
-    (str warn-str crit-str)))
+    (s/trim (str warn-str crit-str))))
 
-(defn- lozenge-value [statuses]
-  (cond-
+(defn lozenge-value [statuses]
+  (cond
     (:crit statuses) {:type "error" :label "ERROR"}
     (:warn statuses) {:type "current" :label "WARN"}
-    :else {:type "success" :label "OK"}
+    :else {:type "success" :label "OK"}))
 
-(defn- status-freqs [events]
+(defn status-freqs [events]
   (frequencies (map #(get status->kw (get-in % [:check :status])) events)))
 
-(defn events->glance-data
-  "Only one glance is handled"
-  [events]
+(defn events->glance-data [events]
   (let [statuses (status-freqs events)]
     {:label {:type "html"
              :value (label-value statuses)}
@@ -41,8 +31,7 @@
      }))
 
 (defn wrap-sensu-glance [content]
-  {:glance [{:key "sensu-glance"
-             :content content}]})
+  {:glance [{:key "sensu-glance" :content content}]})
 
 (defn on-sensu-events [id events]
   (let [installation (inst/lookup id)
