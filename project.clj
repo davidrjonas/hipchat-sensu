23340fab9490069df34444763f1b1b642578f583
David Jonas <djonas@noip.com>
2016-02-17 23:56:26 -0800

    Adds http-kit.fake dev dep

--- project.clj
+++ project.clj
@@ -16,4 +16,5 @@
   :ring {:handler hipchat-sensu.app/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.0"]
+                        [http-kit.fake "0.2.1"]]}})
